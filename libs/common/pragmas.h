3ee2455918aa23aab9f8da5b737e3f11e41175d7
Keith Olsen <keith.a.olsen@gmail.com>
2017-03-19 10:29:24 -0700

    Delete pragmas.h

--- libs/common/pragmas.h
+++
@@ -1,7 +0,0 @@
-//  This file contains #pragmas shared by all the source files
-
-//  Disable warning 4786 (which warns that an identifier has been truncated
-//			  255 characters -- it appears many times when
-//			  building the Debug configuration)
-
-#pragma warning(disable : 4786)
